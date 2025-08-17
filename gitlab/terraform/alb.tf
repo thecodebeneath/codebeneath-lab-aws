c28ae886d409c95267ece9254358ba34929e3aed
thecodebeneath <>
2025-08-17 11:03:47 -0500

    Do the HTTP-to-HTTPS redirect at ALB instead of at the Gitlab instance

--- gitlab/terraform/alb.tf
+++ gitlab/terraform/alb.tf
@@ -11,7 +11,7 @@ resource "aws_lb" "gitlab-alb" {
   name               = "${var.project-name}-gitlab-alb"
   internal           = false
   load_balancer_type = "application"
-    ip_address_type  = "ipv4"
+  ip_address_type    = "ipv4"
   subnets            = [ data.aws_subnet.lab-subnet-2a.id, data.aws_subnet.lab-subnet-2b.id ]
   security_groups    = [ aws_security_group.gitlab-sg.id ]
   connection_logs {
@@ -31,10 +31,13 @@ resource "aws_lb_listener" "port-80" {
   load_balancer_arn = aws_lb.gitlab-alb.arn
   port              = "80"
   protocol          = "HTTP"
-  routing_http_response_server_enabled = "true"
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.gitlab-server.arn
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
 }
 
@@ -56,11 +59,18 @@ resource "aws_lb_listener" "port-443" {
 #   certificate_arn = data.aws_acm_certificate.codebeneath-labs-org.arn
 # }
 
+# resource "aws_acm_certificate" "wildcard-codebeneath-labs-org" {
+#   private_key       = file("./ssh/privkey1.pem")
+#   certificate_body  = file("./ssh/cert1.pem")
+#   certificate_chain = file("./ssh/chain1.pem")
+# }
+
 resource "aws_lb_target_group" "gitlab-server" {
   name     = "gitlab-80"
   port     = 80
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.lab-vpc.id
+  load_balancing_cross_zone_enabled = true
 }
 
 resource "aws_lb_target_group_attachment" "gitlab-server" {
