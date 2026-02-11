70fc019323805cd70818081d7cdb7d53a7c0da98
thecodebeneath <>
2026-02-11 17:06:06 -0600

    Testing how to delete an S3 bucket that is marked as 'prevent_delete=true'

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "prevent_destroy_bucket" {
   bucket = "my-unique-s3-bucket-name-prevent-destroy-12345" # Replace with a globally unique name
 
   lifecycle {
-    prevent_destroy = false
+    prevent_destroy = true
   }
 
   tags = {
